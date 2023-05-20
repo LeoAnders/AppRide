da1b6d0356df6cab26f6e4f8e2f04c028c98d910
Leonardo Anders <leonardoanders15@gmail.com>
2023-05-20 19:44:39 -0300

    Armazenando dados no LocalStorage

--- speedometer.js
+++ speedometer.js
@@ -4,12 +4,14 @@ const startButton = document.querySelector("#start");
 const stopButton = document.querySelector("#stop");
 
 let watchID = null
+let currentRide = null
 startButton.addEventListener("click",() => {
    if(watchID)
       return
 
 //Esta é a função de callback que será chamada quando a posição do dispositivo for capturada com sucesso.
    function handleSuccess(position){
+      addPosition(currentRide,position)
       console.log(position);
       speedElement.innerText = position.coords.speed ?
        (position.coords.speed *3.6).toFixed(1) :0
@@ -21,8 +23,8 @@ startButton.addEventListener("click",() => {
    }
 //Este objeto contém as opções de configuração para a captura da posição do dispositivo. Neste caso, está habilitado o uso da precisão alta para a leitura.   
    const options = { enableHighAccuracy: true}
+   currentRide = createNewRide()//Criando o dado antes de watchPosition
    watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, options)
-
    startButton.classList.add("d-none");
    stopButton.classList.remove("d-none");
 
@@ -32,7 +34,9 @@ stopButton.addEventListener("click",() => {
     if(!watchID)
     return
    navigator.geolocation.clearWatch(watchID)
-   watchID == null
+   watchID = null
+   updateStopTime(currentRide)
+   currentRide = null
    startButton.classList.remove("d-none");
    stopButton.classList.add("d-none");
 
---
+++ storagePosition.js
@@ -0,0 +1,49 @@
+
+function createNewRide(){
+   const rideID = Date.now()//Captura o momento que o usuário aperta em Start
+   const rideRecord = {
+    data: [], // Array para armazenar as posições durante o passeio
+    startTime: rideID,
+    stopTime: null
+
+
+   } 
+   localStorage.setItem(rideID, JSON.stringify(rideRecord))// Armazena o registro do passeio no localStorage
+   return rideID
+
+}
+function saveRideRecord(rideID, rideRecord) {
+   localStorage.setItem(rideID, JSON.stringify(rideRecord))// Atualiza o registro do passeio no localStorage
+}
+
+function getRideRecord(rideID){
+
+   return JSON.parse(localStorage.getItem(rideID))// Obtém o registro do passeio do localStorage e o converte de volta para um objeto
+}
+
+function addPosition(rideID, position){
+   const rideRecord = getRideRecord(rideID)
+
+    // Cria um novo objeto com os dados da posição atual
+   const newData = {
+      accuracy: position.coords.accuracy,
+      altitude:position.coords.altitude,
+      altitudeAccuracy:position.coords.altitudeAccuracy,
+      heading:position.coords.heading, 
+      latitude:position.coords.latitude, 
+      longitude:position.coords.longitude,
+      speed:position.coords.speed,
+      timesTamp: position.timesTamp
+   }
+
+   rideRecord.data.push(newData) // Adiciona os dados da posição ao array de dados do passeio
+   saveRideRecord(rideID, rideRecord) // Salva o registro atualizado do passeio no localStorage
+   
+}
+
+function updateStopTime(rideID){
+   const rideRecord = getRideRecord(rideID)
+   rideRecord.stopTime = Date.now()
+   saveRideRecord(rideID, rideRecord)
+
+}
